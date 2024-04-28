070a3596c0d9fdb473afd6910083418455867c2f
GitHub Action <action@github.com>
2024-04-28 14:30:10 +0000

    Initialize template repository proper
    Set package name to com.github.mpro256.czcxmod
    Set project name to CzcxMod
    Set modid to czcxmod

--- src/main/java/com/github/mpro256/czcxmod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/mpro256/czcxmod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.mpro256.czcxmod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/mpro256/czcxmod/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/mpro256/czcxmod/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.mpro256.czcxmod.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
